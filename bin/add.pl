b57043caa52fd0c2fec98c115c857d5fb44af8e1
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-09-03 14:15:43 +0900

    add copy script

---
+++ bin/add.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use 5.10.0;
+use utf8;
+use strict;
+use warnings;
+use open qw( :utf8 :std );
+use Data::Dumper;
+use Path::Class qw( file dir );
+use File::Copy ( );
+use Time::Piece qw( localtime );
+
+my $original = file( "$ENV{HOME}/Downloads/clip.png" );
+
+die "No copy source found"
+    unless -e $original;
+
+my $filename = get_new_filename( );
+
+File::Copy::copy( $original, $filename )
+    or die "Could not copy from $original to $filename, $!";
+
+exit;
+
+sub get_new_filename {
+    my $dir = dir( "image" );
+    my $date = localtime->date;
+    my $suffix = 1;
+
+    while ( 1 ) {
+        my $basename = join q{.}, ( localtime->date, $suffix != 1 ? ( $suffix ) : ( ), q{png} );
+        my $file = $dir->file( $basename );
+        warn "\$file: $file";
+        return $file
+            if ! -e $file;
+        $suffix++;
+        die "Too many retries: $suffix"
+            if $suffix > 10;
+    }
+}
+
